8e94eefbcbf1ca687ef88b9f8207b80ec2f90b5d
Kiyan <dainguyen0707@gmail.com>
2021-11-29 22:35:22 -0800

    Create handleTimer.js

---
+++ src/functions/handleTimer.js
@@ -0,0 +1,53 @@
+function RunIt(func, delay)
+{
+    if (delay === undefined)
+    {
+        delay = 1000*60 //1 min to avoid
+    }
+
+    //get stuff
+    this.func = func;
+    var times = -1;
+    var timeCount = 0;
+    var ticks = (delay/10)|0;
+    var count = 0;
+    RunIt.instance.push(this);
+
+    this.tick = function()
+    {
+        if (count >= ticks)
+        {
+            eval(this.func);
+            count = 0;
+            if (times > -1)
+            {
+                timeCount++;
+                if (timeCount >= times)
+                {
+                    this.stop();
+                }
+            }
+        }
+        count++;
+    }
+
+    this.stop = function()
+    {
+        var index = RunIt.instance.indexOf(this)
+        RunIt.instance.splice(index, 1);
+    }
+}
+
+RunIt.instances = [];
+
+RunIt.ontick = function ()
+{
+    for(var i in RunIt.instances)
+    {
+        RunIt.instances[i].tick();
+    }
+}
+
+setInterval(RunIt.ontick, 10);
+
+module.exports = (bot) => {RunIt};
